d9ddb96ede3223917834fe18bf7bfa196db58711
肖乔辉 <1187773467@qq.com>
2020-01-03 18:43:56 +0800

    add

--- springcloud-configclient/src/main/java/com/joe/controller/ConfigClientController.java
+++ springcloud-configclient/src/main/java/com/joe/controller/ConfigClientController.java
@@ -1,10 +1,13 @@
 package com.joe.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
  
  
@@ -14,6 +17,11 @@ public class ConfigClientController {
     public String getTestValue(){
  
         return "获取到的配置文件值为："+testValue;
+    }
+
+    @GetMapping("/refresh")
+    public String getRefresh(){
+        return "刷新成功！";
     }
  
 }
