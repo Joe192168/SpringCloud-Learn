b0f153c1908eba3aee65f94a1ce6b33e59bdfc6e
肖乔辉 <1187773467@qq.com>
2020-01-06 15:18:04 +0800

    add

--- springcloud-configclient/src/main/java/com/joe/controller/GitController.java
+++ springcloud-configclient/src/main/java/com/joe/controller/GitController.java
@@ -1,5 +1,7 @@
 package com.joe.controller;
 
+import com.joe.config.GitConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,20 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
-public class ConfigClientController {
- 
- 
-    @Value("${testValue}")
+public class GitController {
+
+    @Autowired
+    private GitConfig gitConfig;
+
+    @GetMapping(value = "show")
+    public Object show(){
+        return gitConfig;
+    }
+
+    @Value("${data.env}")
     String testValue;
     @RequestMapping(value = "/getTestValue")
     public String getTestValue(){
- 
+
         return "获取到的配置文件值为："+testValue;
     }
-
-    @GetMapping("/refresh")
-    public String getRefresh(){
-        return "刷新成功！";
-    }
- 
 }
